53f9d14abc2899bbe955464676113c76d678d478
Deboprasad <deboprasadm@yahoo.com>
2021-04-23 22:23:18 +0530

    Deboprasad-commit

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import react,{Component} from 'react';
+// import ReactDOM from 'react-dom'
+ import './Footer.css'
+
+
+const Footer = () => {
+    return (
+      <div className="footer">
+        <p>All Rights Reserved &copy; 2021</p>
+        </div>
+    )
+}
+
+export default Footer
