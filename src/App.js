359358bc5083ccf79ceaa0a8a2ddb68467e4df01
koushiksaha16021996 <koushiksaha16021996>
2021-04-23 23:41:37 +0530

    Home page final commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import logo from './logo.svg';
 import About from './navBarComponent/About';
 import Developer from './navBarComponent/Developer';
 import ImageSlider from './sliderImage/ImageSlider';
+import NavBar from './Navbar/NavBar'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Error from './error/Error';
 
 function App() {
   return (
     <div>
-        <Developer/>
-        <ImageSlider />
+        <BrowserRouter>
+          <NavBar/>
+          <Switch>
+            <Route exact path="/" component={()=><ImageSlider/>}/>
+            <Route exact path="/about" component={()=><About/>}/>
+            <Route exact path="/developers" component={()=><Developer/>}/>
+            <Route exact component={()=><Error/>}/>
+          </Switch>
+        </BrowserRouter>
+        
     </div>
   );
 }
