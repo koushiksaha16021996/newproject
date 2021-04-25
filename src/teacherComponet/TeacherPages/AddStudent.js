e2341925a5d71e9c781bb73d7e1f769407f283c2
koushiksaha16021996 <koushiksaha16021996>
2021-04-25 18:27:25 +0530

    commit

--- src/teacherComponet/TeacherPages/AddStudent.js
+++ src/teacherComponet/TeacherPages/AddStudent.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function AddStudent() {
     return (
-        <div>
-            <h1>You can Add Student Here.</h1>
+        <div className="container">
+            hi
         </div>
+        
     )
 }
--- src/teacherComponet/TeacherPages/TakeAtten.js
+++ src/teacherComponet/TeacherPages/TakeAtten.js
@@ -1,9 +1,83 @@
 import React from 'react'
 
 export default function TakeAtten() {
+  const students = [
+    {
+      roll: 1,
+      name: "Subhasish Choudhury",
+    },
+    {
+      roll: 2,
+      name: "Debo Prasad",
+    },
+
+    {
+      roll: 3,
+      name: "Prasad mukherjee",
+    },
+
+    {
+      roll: 4,
+      name: "Md Azam",
+    },
+
+    {
+      roll: 5,
+      name: "Koushik Saha",
+    },
+
+    {
+      roll: 6,
+      name: "Indus Net",
+    },
+
+    {
+      roll: 7,
+      name: "Bumrah",
+    },
+
+    {
+      roll: 8,
+      name: "Virat kohli",
+    },
+
+    {
+      roll: 9,
+      name: "Sayan Choudhury",
+    },
+
+    {
+      roll: 10,
+      name: "Suman Choudhury",
+    },
+
+    {
+      roll: 2,
+      name: "Debo Prasad mukherjee",
+    },
+  ];
+  const st={
+    itemAlign:"center",
+    margin: "auto"
+  }
     return (
-        <div>
-            <h1>Attendance Sheet</h1>
+        <div className="container">
+            <table className="table table-striped w-auto" style={st}>
+              <thead>
+                <tr className="table-info">
+                  <th>ROLL</th>
+                  <th>NAME</th>
+                  <th>ATTENDANCE</th>
+                </tr>
+              </thead>
+              <tbody>
+                {students.map(s=><tr>
+                  <th>{s.roll}</th>
+                  <td>{s.name}</td>
+                  <td><button>click to present</button></td>
+                </tr>)}
+              </tbody>
+            </table>
         </div>
     )
 }
