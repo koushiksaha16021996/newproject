75e6cb39246a59b663f77f3acff3fcda379f9720
koushiksaha16021996 <koushiksaha16021996>
2021-04-25 20:53:55 +0530

    koushik commit

--- src/teacherComponet/TeacherPages/TeacherPortal.js
+++ src/teacherComponet/TeacherPages/TeacherPortal.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import FacultyNotice from './FacultyNotice'
+import './Facultyview.css'
 
 export default function TeacherPortal() {
     return (
-        <div>
-            <h1>Teacher Portal</h1>
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-6">
+                    <FacultyNotice/>
+                </div>
+                <div className="col-lg-6">
+                    <div className="aboutss">
+                        <div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
