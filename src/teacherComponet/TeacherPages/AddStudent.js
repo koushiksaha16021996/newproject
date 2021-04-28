a91e3c92785cda2fe7460e1dc62aaac4d8fa0be3
Deboprasad <deboprasadm@yahoo.com>
2021-04-28 18:36:15 +0530

    commit debo

--- src/teacherComponet/TeacherPages/AddStudent.js
+++ src/teacherComponet/TeacherPages/AddStudent.js
@@ -1,35 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import {useHistory} from 'react-router-dom'
 
 export default function AddStudent() {
+  const history= useHistory()
+  const [user, setUser]=useState({
+    "roll":"",
+    "name":"",
+    "email":"",
+    "password":""
+  })
+  let name ,value
+  const handleInput=(e) => {
+    name=e.target.name
+    value=e.target.value
+    setUser({
+      ...user,
+      [name]:value
+    })
+
+  }
+  
+
+  const param=new URLSearchParams();
+  const handleSubmit=()=>{
+    //e.preventDefault();
+   param.append("roll", user.roll); 
+   param.append("name", user.name);
+   param.append("email", user.email);
+   param.append("password", user.password);
+    axios.post("http://localhost:4000/student", param,{
+        headers:{
+            'content-Type': 'application/x-www-form-urlencoded'
+        }
+    }).then(res=>{
+        window.alert("Successfully Added")
+        console.log("ok")
+        history.push("/studentinfo")
+    })
+
+}
     
     return (
         <div className="container" style={{width: "60%", marginLeft: "20%", background: "#f5f5f5", padding: "20px", borderRadius: "10px"}}>
-            <center style={{color: "black", textDecoration: "underline", fontFamily: "Copperplate, Papyrus"}}><h2>ADD or EDIT STUDENT DETAILS</h2></center>
+            <center style={{color: "black", textDecoration: "underline" }}><h2>ADD or EDIT STUDENT DETAILS</h2></center>
             <div className="formbody">
-            <form>
+            <form > 
+
                 <div class="form-group">
                   <label for="exampleInputEmail1" style={{fontWeight: "700"}}>Roll</label>
-                  <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Roll No"/>
+                  <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Roll No" value={user.roll} name="roll" onChange={handleInput}/>
                 </div>
+                
                 <div class="form-group">
                   <label for="exampleInputEmail1" style={{fontWeight: "700"}}>Name</label>
-                  <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Name"/>
+                  <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Name" value={user.name} name="name" onChange={handleInput}/>
                 </div>
+
                 <div class="form-group">
                   <label for="exampleInputEmail1" style={{fontWeight: "700"}}>Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"  value={user.email} name="email" onChange={handleInput}/>
                 </div>
+       
                 <div class="form-group">
-                  <label for="exampleInputPassword1" style={{fontWeight: "700"}}>Address</label>
-                  <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Address"/>
-                </div>
-                <div class="form-group">
-                  <label for="exampleInputPassword1" style={{fontWeight: "700"}}>Initial status</label>
-                  <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Attendance status"/>
+                  <label for="exampleInputEmail1" style={{fontWeight: "700"}}>Password</label>
+                  <input type="password" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="password" value={user.password} name="password" onChange={handleInput}/>
                 </div>
 
                 
-                <center><button type="button" class="btn btn-primary" >Submit</button></center>
+                <center><button type="button" class="btn btn-primary" value="Submit" onClick={()=>handleSubmit()}>Submit</button></center>
             </form>
             </div>
         </div>
