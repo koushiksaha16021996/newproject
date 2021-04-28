7ef8c811395f76e6d7e626e88eb2b14ba71e58e4
koushiksaha16021996 <koushiksaha16021996>
2021-04-28 12:59:57 +0530

    new

--- src/RegisterPage/Register.jsx
+++ src/RegisterPage/Register.jsx
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios'
 import './Register.css';
 
 export default function Register() {
+    const [user,setUser]=useState({
+        "name":"",
+        "email":"",
+        "password":"",
+        "cPassword":"",
+        "accessToken":""
+    })
+    const param=new URLSearchParams();
+    let name, value
+const handleInput=(e)=>{
+        
+        name=e.target.name
+        value = e.target.value
+        setUser({
+            ...user,
+            [name]:value
+        })
+    }
+
+const handlepost=()=>{
+    //e.preventDefault();
+   param.append("name", user.name);
+   param.append("email", user.email);
+   param.append("password", user.password);
+   param.append("cPassword", user.cPassword);
+   param.append("accessToken", user.accessToken);
+    axios.post("http://localhost:4000/user", param,{
+        headers:{
+            'content-Type': 'application/x-www-form-urlencoded'
+        }
+    }).then(res=>{
+        console.log("ok")
+    })
+
+}
+
+
+
+
+
     return (
         <div>
             <form className="RegisterPage">
@@ -11,30 +52,31 @@ export default function Register() {
 
                 <div className="form-group">
                     <label className="RegisterLabel">Name</label>
-                    <input type="text" className="form-control" placeholder="Enter Your Name" />
+                    <input type="text" className="form-control" placeholder="Enter Your Name" name="name" value={user.name} onChange={handleInput}/>
                 </div>
 
                 <div className="form-group">
                     <label className="RegisterLabel">Email</label>
-                    <input type="email" className="form-control" placeholder="Enter your Email." />
+                    <input type="email" className="form-control" placeholder="Enter your Email." name="email" value={user.email} onChange={handleInput}/>
                 </div>
                 <div className="form-group">
                     <label className="RegisterLabel">Password</label>
-                    <input type="password" className="form-control" placeholder="Enter your Password." />
+                    <input type="password" className="form-control" placeholder="Enter your Password." name="password" value={user.password} onChange={handleInput}/>
                 </div>
                 <div className="form-group">
                     <label className="RegisterLabel">Confirm Password</label>
-                    <input type="password" className="form-control" placeholder="Confirm Your Password." />
+                    <input type="password" className="form-control" placeholder="Confirm Your Password." name="cPassword" value={user.cPassword} onChange={handleInput}/>
                 </div>
 
                 <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                    </div>
+                    <label className="RegisterLabel">Access Token</label>
+                    <input type="text" className="form-control" placeholder="Access Token" name="accessToken" value={user.accessToken} onChange={handleInput}/>
                 </div>
 
-                <button type="submit" className="btn btn-dark btn-lg btn-block"><Link to="/register">Sign Up</Link></button>
+
+                
+
+                <button type="button" className="btn btn-dark btn-lg btn-block" onClick={()=>handlepost()}><Link to="/register">Sign Up</Link></button>
                 <p className="forgot-password text-right">
                 Already have an account? <Link to="/loginpage">Sign in</Link>
                 </p>
